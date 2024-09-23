352d94eaf021441177a18cfe86e66b169944c764
Sydney Kibanga <skibanga@aakvatech.com>
2024-09-23 22:37:57 +0300

    feat: Hide the debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -84,6 +84,7 @@ class MyApp extends StatelessWidget {
         primaryColor: primaryColor,
         fontFamily: 'Montserrat',
       ),
+      debugShowCheckedModeBanner: false,
       home: const LoginScreen(),
     );
   }
